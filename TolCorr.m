3c3648aa0e29310b04907bc62d90b98d53f00573
AlexanderBazhenov <59746051+AlexanderBazhenov@users.noreply.github.com>
2024-10-02 16:50:39 +0300

    Add files via upload

--- TolCorr.m
+++ TolCorr.m
@@ -10,7 +10,8 @@ dir2023 ='e:\Users\Public\Documents\ST\2023\T\'
 dirki ='e:\Users\Public\Documents\ST\2024\T\kinterval-0.0.1\'
 % 2024-08-12
 dirData = 'e:\Users\Public\Documents\ST\2024\T\icamp2025\'
-% 2024-08-22
+% 2024-10-02
+dirData = 'e:\Users\Public\Documents\ST\2024\T\icamp2025v2\'
 
 % 2024-08-23
 % HomePC
@@ -24,7 +25,7 @@ addpath(dirroot)
 addpath(dir2023)
 addpath(dirki)
 
-cd(dirData), pwd
+cd(dirroot), pwd
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% bin ch %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 pkg load interval
@@ -35,8 +36,18 @@ dirlinprogpoly = 'e:\Users\Public\Documents\ST\2024\T\octave-interval\m\polytope
 %dirlinprogpoly = 'D:\ST\2024\T\octave-interval\m\polytopes\'
 addpath(dirlinprogpoly)
 %
+dir2D = 'e:\Users\Public\Documents\ST\2024\T\IntLinInc2D\'
+%dirlinprogpoly = 'D:\ST\2024\T\octave-interval\m\polytopes\'
+addpath(dir2D)
+%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 2024-09-30
 icamp2025data
+% 2024-10-02
+icamp2025datav2
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 
  [V,P1,P2,P3,P4]=EqnTol2D(xx4opt,xx4opt,b_opt_left4,b_opt_rigth4)
  Part_SSW_color(P1,P2,P3,P4, [ 0 1 0])
@@ -71,7 +82,7 @@ b_in = ir_outer(irp_T2025_in)
 % external
 y_ex = (b_ex_left + b_ex_rigth)/2;
 epsilon_ex = (-b_ex_left + b_ex_rigth)/2;
-irp_T2025_ex = ir_problem(xx, y_ex, epsilon);
+irp_T2025_ex = ir_problem(xx, y_ex, epsilon_ex);
 b_ex = ir_outer(irp_T2025_ex);
 
 figure
@@ -101,6 +112,7 @@ epsilon_opt= (-b_opt_left4 + b_opt_rigth4)/2;
 irp_T2025_opt = ir_problem(xx4opt, y_opt, epsilon_opt);
 ir_plotbeta(irp_T2025_opt)
 titlestr=strcat('Information set', ' icamp2025=', ' ex', ' easy', ' opt')
+titlestr=strcat('Information set', ' icamp2025v2=', ' ex', ' easy', ' opt')
 title(titlestr)
 figure_name_out=strcat(titlestr, '.png')
 print('-dpng', '-r300', figure_name_out), pwd
@@ -130,17 +142,15 @@ pcolor = [0.7 0.7 0.9]
 ##ir_scatter(irp_T2025_in,'b.')
 ##ir_scatter(irp_T2025_ex,'bo')
 
-h4 = errorbar(xx(:,1), y_ex, epsilon_ex,'.r');
-h5 = errorbar(xx(:,1), y_in, epsilon_in,'.b');
+h4 = errorbar(xx(:,1), y_ex, epsilon_ex,'.k');
+%h5 = errorbar(xx(:,1), y_in, epsilon_in,'.b');
+h5 = errorbar(xx(:,1), y_easy, epsilon_easy,'.b');
+h6 = errorbar(xx4opt(:,1), y_opt, epsilon_opt,'.m');
 
-
-lgd123 = legend([h1 h2 h3 h4 h5], ...
-  {'ex', 'easy', 'opt', 'ex dots', 'in dots'})
+lgd123 = legend([h1 h2 h3 h4 h5 h6], ...
+  {'ex', 'easy', 'opt', 'ex errors', 'easy errors', 'opt errors'})
 set(lgd123, 'location', 'north')
-set(lgd123, 'fontsize', 12)
-
-
-
+set(lgd123, 'fontsize', 14)
 
 xticks([1:9])
 
@@ -148,10 +158,10 @@ xticks([1:9])
 %ir_scatter(irp_T2025_opt,'kx')
 
 grid on
-set(gca, 'fontsize', 12)
+set(gca, 'fontsize', 14)
 xlabel('number')
 ylabel('data')
-titlestr = 'Set of models compatible with data and constraints'
+titlestr = strcat('Set of models compatible with data and constraints', ' Tokyo2025 v2')
 title(titlestr)
 figure_name_out=strcat(titlestr, '.png')
 print('-dpng', '-r300', figure_name_out), pwd
